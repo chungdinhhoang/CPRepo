0a3f9984e844a96cbafd6e5627f13f835cc27bd5
chungdinhhoang <hgchung.dinh@gmail.com>
2021-11-27 10:23:30 +0700

    Just rewrite the code.

--- Graphs/bridge.cpp
+++ Graphs/bridge.cpp
@@ -1,63 +1,56 @@
 #include <bits/stdc++.h>
 
-#define ii pair<int,int>
-
 using namespace std;
 
-const int N = 3e5;
+const int N = 1e5;
 
-int n,m;
+int n, m;
 vector<int> g[N];
 
 int tin[N], low[N];
-int dfsTime=0;
+int dfsTime = 0;
 
-void dfs(int u, int pre);
+bool articulation_point[N];
+int bridge_count = 0;
 
-int main(){
-    freopen("main.inp","r",stdin);
-    memset(tin,0,sizeof tin);
 
-    //Find bridges and articulation points
+void dfs(int u, int pre = -1) {
+    tin[u] = low[u] = ++dfsTime;
+    int child = 0;
 
-    // initialize the graph
-    cin >> n >> m;
-    while (m--) {
-        int a,b;cin>>a>>b;
-        g[a].push_back(b);
-        g[b].push_back(a);
-    }
-
-    for (int u=1;u<=n;u++){
-        if (!tin[u]) dfs(u,-1);
-    }
-}
-
-void is_bridge(int u,int v){
-    printf("%d - %d.\n",u,v);
-}
-
-void is_articulation_point(int u){
-    printf("%d is an articulation point.\n",u);
-}
-
-void dfs(int u, int pre){
-    tin[u]=low[u]=++dfsTime;
-    int child=0; // just used for the root of the dfs tree
-
-    for(int v : g[u]){
+    for (int v : g[u]) {
         if (v == pre) continue;
-
-        if (!tin[v]){
-           // cout<<v<<endl;
-            dfs(v,u);
-            low[u]=min(low[u],low[v]);
+        if (tin[v]) low[u] = min(low[u], tin[v]);
+        else {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
             child++;
 
-            if (low[v] > tin[u]) is_bridge(u,v);
-            if (low[v] >= tin[u] && pre != -1) is_articulation_point(u); // pre != -1 is an important condition.
-        } else low[u]=min(low[u],tin[v]);
+            if (low[v] > tin[u]) bridge_count++;
+            if (low[v] >= tin[u] && pre != -1) articulation_point[u] = true;
+        }
     }
 
-    if (pre == -1 && child >= 2) is_articulation_point(u); //corner case
+    if (pre == -1 && child >= 2) articulation_point[u] = true;
+}
+
+int main() {
+    #ifdef CHUNGDINH
+    freopen("main.inp", "r", stdin);
+    #endif
+
+    cin >> n >> m;
+    while (m--) {
+        int u, v; cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    memset(tin, 0, sizeof tin);
+
+    for (int i = 1; i <= n; i++) if (!tin[i]) dfs(i);
+
+    int articulation_point_count = 0;
+    for (int i = 1; i <= n; i++) articulation_point_count += (articulation_point[i]);
+
+    cout << articulation_point_count << " " << bridge_count;
 }
