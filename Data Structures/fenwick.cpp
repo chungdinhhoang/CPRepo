d438671281d961bb634b69cbd5e7544c0cfea182
DANG NGUYEN BAO <96303529+bibimoni@users.noreply.github.com>
2024-01-14 17:07:28 +0700

    Create fenwick.cpp

---
+++ Data Structures/fenwick.cpp
@@ -0,0 +1,27 @@
+template <class T>
+class fenwick {
+  vector<T> a;
+  int n;
+
+public:
+  fenwick() {}
+
+  fenwick(int n_) : n(n_) {
+    // check initial value
+    a.assign(n + 5, static_cast<T>(0));
+  }
+
+  T query(int r) {
+    T q = 0; // check default value of query
+    for (; r > 0; r -= (r & -r)) {
+      q += a[r]; // check query modification
+    }
+    return q;
+  }
+
+  void update(int r, T del) {
+    for (; r <= n; r += (r & -r)) {
+      a[r] += del; //check update rule
+    }
+  }
+};
