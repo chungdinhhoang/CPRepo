a82b0b40744b306b7f92b2e2942ff26772d889e9
DANG NGUYEN BAO <96303529+bibimoni@users.noreply.github.com>
2024-01-14 19:02:00 +0700

    Update modint.cpp

--- Mathematics/modint.cpp
+++ Mathematics/modint.cpp
@@ -1,32 +1,32 @@
 /* 
  * Caution:
- *   *need to cast into an int before printing
+ *   * need to cast into int before printing
  * source: Benq
 */
-struct modint {
+struct Mint {
   int v;
   explicit operator int() const { return v; }
-  modint() { v = 0; }
-  modint(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
+  Mint() { v = 0; }
+  Mint(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
 };
-modint &operator+=(modint &a, modint b) {
+Mint &operator+=(Mint &a, Mint b) {
   if ((a.v += b.v) >= MOD) a.v -= MOD;
   return a;
 }
-modint &operator-=(modint &a, modint b) {
+Mint &operator-=(Mint &a, Mint b) {
   if ((a.v -= b.v) < 0) a.v += MOD;
   return a;
 }
-modint operator+(modint a, modint b) { return a += b; }
-modint operator-(modint a, modint b) { return a -= b; }
-modint operator*(modint a, modint b) { return modint((long long)a.v * b.v); }
-modint &operator*=(modint &a, modint b) { return a = a * b; }
-modint pow(modint a, long long p) {
+Mint operator+(Mint a, Mint b) { return a += b; }
+Mint operator-(Mint a, Mint b) { return a -= b; }
+Mint operator*(Mint a, Mint b) { return Mint((long long)a.v * b.v); }
+Mint &operator*=(Mint &a, Mint b) { return a = a * b; }
+Mint pow(Mint a, long long p) {
   assert(p >= 0);
   return p == 0 ? 1 : pow(a * a, p / 2) * (p & 1 ? a : 1);
 }
-modint inv(modint a) {
+Mint inv(Mint a) {
   assert(a.v != 0);
   return pow(a, MOD - 2);
 }
-modint operator/(modint a, modint b) { return a * inv(b); }
+Mint operator/(Mint a, Mint b) { return a * inv(b); }
