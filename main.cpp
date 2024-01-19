3601ba97cc16b7cafc00738f08d098668ad63d50
chungdinhhoang <hgchung.dinh@gmail.com>
2024-01-19 11:49:04 +0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,30 @@
-#include <bits/stdc++.h>
-
 #define ll long long
 #define ii pair<long long, long long>
 #define fi first
 #define se second
 #define MP make_pair
-
-using namespace std;
+#define sz(x) (long long)(x.size())
+#define all(x) x.begin(), x.end()
+#define mmax(a, b) a = max(a, b)
+#define mmin(a, b) a = min(a, b)
+#define For(i,l,r) for (long long i = (l); i <= (r); i++)
+#define Ford(i,r,l) for (long long i = (r); i >= (l); i--)
 
 const int N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e18;
 const int iINF = 1e9;
 
-void solve() {
-}
-
-int main(){
-    #ifdef CHUNGDINH
-    freopen("main.inp", "r", stdin);
-    #endif // CHUNGDINH
-
-    cout << fixed << setprecision(8);
-
-    int t = 1;
-    //cin >> t;
-    while (t--) solve();
-}
-
-
 /*
 Check before submitting:
++ corner cases (n=1,n=0,full of 1,full of 0,)
++ use fastio
++ check input output type (stdin or file)
 + Array bounds ( segmentation fault)
 + int or long long
 + reset all the variables (multiple tests)
 + is the graph connected (may have > 1 connected components)
++ reindex the string => s = "#" + s;
++ use 1ll << x, not 1 << x
++ while using board, be careful with m and n
 */
