b1b8a39c2148354b4b79ea663ef214a38bbd1767
chungdinhhoang <hgchung.dinh@gmail.com>
2024-01-31 09:00:51 +0700

    delete SparseTable.cpp modified Point-Line.cpp

--- mathematics/Point-Line.cpp
+++ mathematics/Point-Line.cpp
@@ -1,15 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define gcd(x, y) __gcd(x, y)
-#define ll long long
-#define ii pair<int, int>
-
-const int N = 5e5 + 5;
-const ll MOD = 1e9 + 7;
-const int iINF = 1e9;
-
 #define pt pair<long long, long long>
 #define x first
 #define y second
@@ -40,11 +28,4 @@ int dir(pt a, pt b, pt c) {
     if (x == 0) return 0;
     else if (x > 0) return 1;
     else return 2;
-}
-
-int main() {
-    #ifdef CHUNGDINH
-    freopen("main.inp", "r", stdin);
-    //freopen("main.out", "w", stdout);
-    #endif // CHUNGDINH
 }
