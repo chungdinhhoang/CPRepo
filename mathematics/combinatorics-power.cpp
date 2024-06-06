61071fa956153bba0dac9a4a7dcf4915f4875ad8
DANG NGUYEN BAO <96303529+bibimoni@users.noreply.github.com>
2024-06-06 17:09:38 +0700

    Create combinatorics-power.cpp

---
+++ mathematics/combinatorics-power.cpp
@@ -0,0 +1,31 @@
+vector<Mint> fact(1, 1);
+vector<Mint> inv_fact(1, 1);
+ 
+template<int M, typename U>
+modnum<M> power(const modnum<M>& a, const U& b) {
+  assert(b >= 0);
+  modnum<M> x = a, res = 1;
+  U p = b;
+  while (p > 0) {
+    if (p & 1) res *= x;
+    x *= x;
+    p >>= 1;
+  }
+  return res;
+}
+ 
+Mint C(int n, int k) {
+  if (k < 0 || k > n) {
+    return 0;
+  }
+  k = min(k, n - k);
+  while ((int) fact.size() < k + 1) {
+    fact.push_back(fact.back() * (int) fact.size());
+    inv_fact.push_back(1 / fact.back());
+  }
+  Mint ret = inv_fact[k];
+  for (int i = 1; i <= k; i++) {
+    ret *= n - i + 1;
+  }
+  return ret;
+}
