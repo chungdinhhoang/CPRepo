960597ea906db22f03e1bb5c0eaac509674f8551
DANG NGUYEN BAO <96303529+bibimoni@users.noreply.github.com>
2024-09-15 23:52:00 +0700

    Create rng.cpp

---
+++ rng.cpp
@@ -0,0 +1,6 @@
+mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+inline long long rnd(long long l, long long r) { 
+  uniform_int_distribution<long long> dis(l, r); 
+  return dis(rng); 
+}
